e8aa307f8da638f5c3444d8398132c2e14b53bc8
Matthias Jansen <matthias.jansen@outlook.de>
2017-04-28 08:33:40 +0200

    Updated the demo;

--- LINQ.Demo/Entry.cs
+++ LINQ.Demo/Entry.cs
@@ -9,22 +9,64 @@ namespace LINQ.Demo
     {
         public static void Main(string[] args)
         {
-            var users = new UserProvider().Provide();
+            //ShowFilteredAndGrouped();
+            ShowImmutability();
 
-            var italians = users.Where(u => u.Country == "Italy");
+            Console.ReadKey();
+        }
 
-            foreach (var italian in italians.GroupBy(i => i.Zip.First()).OrderBy(g => g.Key))
+        private static void ShowFilteredAndGrouped()
+        {
+            var itaGroups = FilterAndGroupingA();
+            //var itaGroups = FilterAndGroupingB();
+
+            foreach (var itaGroup in itaGroups)
             {
-                Console.WriteLine(italian.Key);
-                
-                foreach (var user in italian)
+                Console.WriteLine(itaGroup.Key);
+
+                foreach (var user in itaGroup)
                 {
                     Console.Write("\t");
                     Console.WriteLine(user);
                 }
             }
+        }
 
-            Console.ReadKey();
+        private static IEnumerable<IGrouping<char, User>> FilterAndGroupingA()
+        {
+            var itaGroups = 
+                from user in new UserProvider().Provide()
+                where user.Country == "Italy"
+                group user by user.Zip.First()
+                into g
+                orderby g.Key
+                select g;
+
+            return itaGroups;
+        }
+
+        private static IEnumerable<IGrouping<char, User>> FilterAndGroupingB()
+        {
+            var users = new UserProvider().Provide();
+
+            var itaGroups = users
+                .Where(u => u.Country == "Italy")
+                .GroupBy(i => i.Zip.First()).OrderBy(g => g.Key);
+
+            return itaGroups;
+        }
+
+        private static void ShowImmutability()
+        {
+            var numbers = Enumerable.Range(0, 10);
+            Console.WriteLine($"Numbers in the original sequence: {numbers.Count()}");
+            var filter1 = numbers.Where(n => n > 4);
+            Console.WriteLine($"Remaining numbers: {filter1.Count()}");
+            var filter2 = filter1.Where(n => n > 4);
+            Console.WriteLine($"Remaining numbers: {filter2.Count()}");
+
+
+            Console.WriteLine($"Are these two enumerables reference equal? {(Equals(filter1, filter2) ? "yep" : "nah")}");
         }
     }
 }
